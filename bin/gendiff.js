5a6ca2da13f64c133e348accf2a217102de563d6
kaguya3222 <maksimanisimov322@gmail.com>
2022-01-24 20:22:37 +0200

    feat: added arguments for cli

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,8 +3,10 @@
 const { Command } = require('commander');
 const program = new Command();
 
-program.version('1.0.0');
-
-program.description('Compares two configuration files and shows a difference.')
+program
+    .version('1.0.0')
+    .arguments('<filepath1> <filepath2>')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
 
 program.parse();
