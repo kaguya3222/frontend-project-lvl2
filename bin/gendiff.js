82848b91293e5c327911bf14c0ac27116f1d9a49
kaguya3222 <maksimanisimov322@gmail.com>
2022-01-25 14:51:13 +0200

    refactor: refactored variable names in bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,8 +10,8 @@ program
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+  .action((firstFilePath, secondFilePath) => {
+    console.log(genDiff(firstFilePath, secondFilePath));
   });
 
 program.parse();
