2ad2d4332482fed1bfb24f217b4b3e98c2538e86
kaguya3222 <maksimanisimov322@gmail.com>
2022-01-30 19:12:34 +0200

    refactor: use path.resolve in tests for getting fixtures

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,8 +1,15 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import genDiff from '../src/genDiff.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
 
 describe('genDiff', () => {
   test('prints difference between two .json files', () => {
-    const diff = genDiff('__fixtures__/first.json', '__fixtures__/second.json'); // TODO: generate absolute path using resolve
+    const diff = genDiff(getFixturePath('first.json'), getFixturePath('second.json'));
     const expectedDiff = `{ 
   - follow: false 
     host: hexlet.io 
